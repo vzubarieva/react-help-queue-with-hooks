96b89f59b7f4f35b5fbfb2120b8924a5fa2a6ee6
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-11-13 14:32:56 -0800

    add styled componenets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const HelpQueueHeader = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  color: white;
+  background-color: purple;
+`;
+
+const StyledWrapper = styled.section`
+  background-color: orange;
+`;
 
 function Header() {
   return (
-    <React.Fragment>
-      <h1> Help Queue</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/sign-in">Sign In</Link>
-        </li>
-      </ul>
-    </React.Fragment>
+    <StyledWrapper>
+      <React.Fragment>
+        <HelpQueueHeader>Help Queue</HelpQueueHeader>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/sign-in">Sign In</Link>
+          </li>
+        </ul>
+      </React.Fragment>
+    </StyledWrapper>
   );
 }
 
