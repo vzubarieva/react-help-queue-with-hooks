52e3c5a7cfa959342bb5454d35920b9c4c88a83c
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-11-13 10:58:16 -0800

    add authentication

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -4,7 +4,7 @@ import TicketList from "./TicketList";
 import EditTicketForm from "./EditTicketForm";
 import TicketDetail from "./TicketDetail";
 import { useState } from "react";
-import db from "./../firebase.js";
+import { db, auth } from "./../firebase.js";
 import {
   collection,
   addDoc,
